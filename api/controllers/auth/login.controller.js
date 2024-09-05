dd58878021d51e73ea24d299c657c76f67fbb4a8
Nitish Uprety <nitish@broadwayinfosys.edu.np>
2024-09-05 08:28:25 +0545

    5th: Ninth commit

--- api/controllers/auth/login.controller.js
+++ api/controllers/auth/login.controller.js
@@ -1,6 +1,33 @@
+const { errorMsg, validationError } = require("../../lib")
+const { User } = require("../../models")
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 class LoginCtrl {
     login = async (req, res, next) => {
-        res.send(req.body)
+        try {
+            const { email, password } = req.body
+
+            const user = await User.findOne({email})
+
+            if(user) {
+                if(bcrypt.compareSync(password, user.password)) {
+                    const token = jwt.sign({
+                        uid: user._id,
+                        iat: Math.floor(Date.now() / 1000),
+                        exp: Math.floor(Date.now() / 1000) + (30*24*60*60),
+                    }, process.env.JWT_SECRET)
+
+                    res.send({ token })
+                } else {
+                    validationError(next, {password: 'Incorrect password'})
+                }
+            } else {
+                validationError(next, {email: "Given email is not registered"})
+            }
+        } catch(error) {
+            errorMsg(error, next)
+        }
     }
 }
 
--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const router = require('./routes')
+const { config } = require('dotenv')
+
+config()
 
 const app = express()
 
