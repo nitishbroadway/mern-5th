f87035e6dbff409a2ac13f710b1a840dd5fa55f9
Nitish Uprety <nitish@broadwayinfosys.edu.np>
2024-09-27 08:36:01 +0545

    5th: Fourteenth commit

--- api/controllers/cms/articles.controller.js
+++ api/controllers/cms/articles.controller.js
@@ -1,5 +1,6 @@
 const { errorMsg, notFoundMsg } = require("../../lib")
 const { Article } = require("../../models")
+const { unlinkSync } = require('node:fs')
 
 class ArticlesCtrl {
     index = async (req, res, next) => {
@@ -15,8 +16,14 @@ class ArticlesCtrl {
     store = async (req, res, next) => {
         try {
             const { name, status, categoryId, content } = req.body
+            
+            let image = null
 
-            await Article.create({ name, status, categoryId, content })
+            if(req.file) {
+                image = req.file.filename
+            }
+
+            await Article.create({ name, status, categoryId, content, image })
 
             res.status(201).send({
                 message: 'Article created'
@@ -45,10 +52,22 @@ class ArticlesCtrl {
     update = async (req, res, next) => {
         try {
             const { id } = req.params
-            const { name, status } = req.body
+            const { name, status, content, categoryId } = req.body
 
-            if (await Article.findById(id)) {
-                await Article.findByIdAndUpdate(id, { name, status })
+            const article = await Article.findById(id)
+
+            if (article) {
+                let image = article.image
+
+                if (req.file) {
+                    image = req.file.filename
+
+                    if(article.image) {
+                        unlinkSync(`./uploads/${article.image}`)
+                    }
+                }
+
+                await Article.findByIdAndUpdate(id, { name, status, categoryId, content, image })
 
                 res.send({
                     message: 'Article updated'
@@ -65,7 +84,13 @@ class ArticlesCtrl {
         try {
             const { id } = req.params
 
-            if (await Article.findById(id)) {
+            const article = await Article.findById(id)
+
+            if (article) {
+                if (article.image) {
+                    unlinkSync(`./uploads/${article.image}`)
+                }
+                
                 await Article.findByIdAndDelete(id)
 
                 res.send({
