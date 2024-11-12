295b623590587030a5d8317e375bb702e2eb90e8
Nitish Uprety <nitish@broadwayinfosys.edu.np>
2024-11-12 08:31:38 +0545

    5th: Fifteenth commit

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,9 @@
 const { Router } = require('express')
 const authRoutes = require('./auth.routes')
 const profileRoutes = require('./profile.routes')
+const frontRoutes = require('./front.routes')
 const cmsRoutes = require('./cms')
-const { auth } = require('../lib')
+const { auth, notFoundMsg } = require('../lib')
 
 const router = Router()
 
@@ -10,5 +11,11 @@ router.use('/auth', authRoutes)
 router.use('/profile', auth, profileRoutes)
 
 router.use('/cms', auth, cmsRoutes)
+
+router.use(frontRoutes)
+
+router.use((req, res, next) => {
+    notFoundMsg(next, 'URL')
+})
 
 module.exports = router
