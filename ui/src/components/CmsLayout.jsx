87e43c67b3adb59f2111c0dd4f34a97ae8243ca9
Nitish Uprety <nitish@broadwayinfosys.edu.np>
2024-11-19 09:15:41 +0545

    5th: Sixteenth commit

---
+++ ui/src/components/CmsLayout.jsx
@@ -0,0 +1,9 @@
+import { Outlet } from "react-router-dom"
+
+export const CmsLayout = () => {
+    return <div>
+        <h1>Layout</h1>
+
+        {/* <Outlet /> */}
+    </div>
+}
---
+++ ui/src/routes/AppRouter.jsx
@@ -0,0 +1,13 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import { Cms } from "../pages"
+import { CmsLayout } from "../components/CmsLayout"
+
+export const AppRouter = () => {
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/cms" element={<CmsLayout />}>
+                <Route index element={Cms.Dashboard.Home} />
+            </Route>
+        </Routes>
+    </BrowserRouter>
+}
